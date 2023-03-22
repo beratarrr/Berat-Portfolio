9595293060d416d2128227ff00b7e67485c9b18b
Berat Aras <b.p.aras@hotmail.com>
2023-03-22 19:17:04 +0100

    s

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  
+  target: 'static',
   components: [
     { path: '~/components/homepage'},
     { path: '~/components/skills'},
