c9996cd0f83c2c79565409c8d01dc84c0db8488f
Berat Aras <b.p.aras@hotmail.com>
2023-03-22 17:30:48 +0100

    very basic navbar, will need improving later

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,15 +1,52 @@
 <template>
-    <nav class="flex h-12 bg-nocta-5 w-full rounded-lg justify-around items-center font-semibold"> 
-        <NuxtLink :class="homeRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="/">Home</NuxtLink>
-        <NuxtLink :class="skillsRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="skills">Skills</NuxtLink>
-        <NuxtLink :class="experienceRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="experience">Experience</NuxtLink>
-        <NuxtLink :class="projectsRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="projects">Projects</NuxtLink>
-        <NuxtLink :class="contactRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="contact">Contact</NuxtLink>
-    </nav>
+    <div>
+        <div class="hidden md:block">
+            <nav class="flex h-12 bg-nocta-5 w-full rounded-lg justify-around items-center font-semibold"> 
+                <NuxtLink :class="homeRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="/">Home</NuxtLink>
+                <NuxtLink :class="skillsRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="skills">Skills</NuxtLink>
+                <NuxtLink :class="experienceRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="experience">Experience</NuxtLink>
+                <NuxtLink :class="projectsRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="projects">Projects</NuxtLink>
+                <NuxtLink :class="contactRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="contact">Contact</NuxtLink>
+            </nav>
+        </div>
+        
+        <div class="md:hidden block top-0">
+            <div class="h-14 bg-nocta-7">
+                <div class="flex items-center justify-end py-4 pr-4">
+                    <button
+                    class="justify-center cursor-pointer h-6 flex lg:hidden flex-col"
+                    @click="hamburgerOpen = !hamburgerOpen">
+                        <span
+                        class="absolute block w-8 h-1 bg-nocta-4 rounded transform transition ease-in-out duration-500 "
+                        :class="{'rotate-45 bg-red-600':hamburgerOpen, '-translate-y-2.5 bg-nocta-4': !hamburgerOpen }"/>
+                        <span
+                        class="block w-8 h-1 rounded transform transition ease-in-out duration-500 bg-nocta-4"
+                        :class="{'opacity-0 ':hamburgerOpen}"/>
+                        <span
+                        class="absolute block w-8 h-1 bg-nocta-4 rounded transform transition ease-in-out duration-500 "
+                        :class="{'-rotate-45 bg-red-600':hamburgerOpen, 'translate-y-2.5 bg-nocta-4': !hamburgerOpen }"/>
+                    </button>
+                </div>
+            </div>
+            <nav class="flex flex-col bg-nocta-5 items-center font-semibold transitiones"
+                :class="{'':hamburgerOpen, 'hidden': !hamburgerOpen }"> 
+                <NuxtLink :class="homeRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="/">Home</NuxtLink>
+                <NuxtLink :class="skillsRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="skills">Skills</NuxtLink>
+                <NuxtLink :class="experienceRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="experience">Experience</NuxtLink>
+                <NuxtLink :class="projectsRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="projects">Projects</NuxtLink>
+                <NuxtLink :class="contactRoute() ? 'activeLink' : 'inactiveLink'" class="pb-1" to="contact">Contact</NuxtLink>
+            </nav>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 export default {
+    data(){
+        return{
+            hamburgerOpen: false
+        }
+    },
     setup () {
         const route = useRoute()
         return {
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,31 @@
 <template>
-    <div class="grid grid-cols-12 grid-rows-6 h-screen w-full fixed bg-gradient-to-tr from-nocta-1   to-nocta-7">
-        <div class="col-span-10 col-start-2 row-span-1 mt-20">
+  <div>
+    <div class="hidden md:block">
+      <div class="grid grid-cols-12 grid-rows-6 h-screen w-full fixed bg-gradient-to-tr from-nocta-1   to-nocta-7">
+          <div class="col-span-10 col-start-2 row-span-1 mt-20">
+              <navbar class=""></navbar>
+          </div>
+          <div class="bg-nocta-5 col-span-10 col-start-2 row-span-4 relative rounded-xl overflow-y-auto scrollbar-hide z-50">
+              <slot class="" />
+          </div>
+          <div class="col-span-2 col-start-6">
+              <Dropdown class=""></Dropdown>
+          </div>
+      </div>
+    </div>
+
+
+    <div class="md:hidden block">
+      <div class="flex flex-col">
+        <div class="">
             <navbar class=""></navbar>
         </div>
-        <div class="bg-nocta-5 col-span-10 col-start-2 row-span-4 relative rounded-xl overflow-y-auto scrollbar-hide z-50">
+        <div class="bg-nocta-5 scrollbar-hide">
             <slot class="" />
         </div>
-        <div class="col-span-2 col-start-6">
-            <Dropdown class=""></Dropdown>
-        </div>
+      </div>
     </div>
+  </div>
 </template>
 <style>
 /* For Webkit-based browsers (Chrome, Safari and Opera) */
