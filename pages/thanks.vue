a2bfca381b55a8519d8ed19e032b6521d839115f
beratarrr <b.p.aras@hotmail.com>
2023-04-02 00:17:11 +0200

    build error fix

--- pages/thanks.vue
+++ pages/thanks.vue
@@ -4,6 +4,6 @@
             <p>Thanks for filling out the form!</p>
             <p class="md:text-xl text-base font-normal text-berry-4">I'll contact you within 24 hours for a nice cup of coffee.</p>
         </page-title>
-        <img src="checkmark.svg" />
+        <img src="/checkmark.svg" />
     </div>
 </template>
